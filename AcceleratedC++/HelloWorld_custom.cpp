26c817b6153d1fc6c387bd48c02d0256ba42b8b6
Samy Hamlaoui <samy.hamlaoui@gmail.com>
2015-12-13 11:10:32 -0600

    Accelerated C++ - Working with strings (Custom Hello World)

---
+++ AcceleratedC++/HelloWorld_custom.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main () {
+
+  // ask for the person's name
+  std::cout << "Please enter your first name: ";
+
+  // read the name
+  std::string name;
+  std::cin >> name ;
+
+  std::cout << "Hello, " << name << "!" << std::endl;
+
+}
